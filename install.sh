0ffa977551a20ebb9e8fcbb236ae06a3cb7809ea
Anska <103422661+0011101100101001@users.noreply.github.com>
2026-07-21 15:30:51 +0200

    feat(install): add dotfiles binary setup

--- install.sh
+++ install.sh
@@ -31,18 +31,16 @@ fi
 
 git clone https://github.com/0011101100101001/dotfiles.git "$DOTFILES_DIR"
 
+echo
 cd "$DOTFILES_DIR"
 CONFIG_DIR="$HOME/.config"
 mkdir -p "$CONFIG_DIR"
 for config_category in editor shell terminal; do
   for config in "$config_category"/*; do
 
-    if [[ $config_category != "shell" ]]; then
-      config=$(basename "$config")
-    fi
-
+    config=$(basename "$config")
     CONFIG_PATH="$CONFIG_DIR/$config"
-    if [[ -d "$CONFIG_PATH" ]]; then
+    if [[ -d "$CONFIG_PATH" || -f "$CONFIG_PATH" ]]; then
       echo -e \
         "$config already present, which action to perform? [o/b/p]\n" \
         "   o: override\n" \
@@ -80,6 +78,8 @@ case "$(basename "$SHELL")" in
   if ! grep "alias.conf" "$SHELL_RC"; then
     echo "source $DOTFILES_DIR/shell/alias.conf" >>"$SHELL_RC"
     source "$SHELLRC"
+  else
+    echo "Alias already setup "
   fi
   ;;
 
@@ -90,3 +90,66 @@ case "$(basename "$SHELL")" in
   fi
   ;;
 esac
+
+echo
+DOTFILES_BIN_DIR="$HOME/.local/bin"
+DOTFILES_BIN="$DOTFILES_BIN_DIR/dotfiles"
+if command -v dotfiles >/dev/null; then
+  echo -e \
+    "Dotfiles binary already installed, override it? [y/n]\n" \
+    "   y: yes\n" \
+    "   n: no"
+  while true; do
+    read -r answer </dev/tty
+    case "$answer" in
+    y | Y)
+      rm -f "$DOTFILES_BIN"
+      break
+      ;;
+
+    n | N)
+      exit 0
+      ;;
+    esac
+  done
+fi
+
+echo -e "Installing dotfiles binary...\n"
+mkdir -p "$DOTFILES_BIN_DIR"
+echo \
+  '#!/bin/bash
+
+USAGE=\
+"Usage: dotfiles [option]
+  i: install
+  u: update
+"
+
+if [[ $# -ne 1 ]]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+DOTFILES_DIR="$HOME/.dotfiles"
+case "$1" in
+  i)
+    curl -fsSL https://raw.githubusercontent.com/0011101100101001/dotfiles/main/install.sh | bash 
+    ;;
+
+  u)
+    cd "$DOTFILES_DIR"
+    git pull
+    ;;
+
+  default)
+    echo "Invalid option: $1"
+    echo "$USAGE"
+    exit 1
+    ;;
+esac
+
+' >"$DOTFILES_BIN"
+
+chmod u+x "$DOTFILES_BIN"
+
+echo "Dotfiles installation done!"
