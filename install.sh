c618f8628212e791527856afd18295c97d7e75f5
Anska <103422661+0011101100101001@users.noreply.github.com>
2026-08-15 18:34:06 +0200

    feat(install): implement ai install logic, start to split categories with their own special cases

--- install.sh
+++ install.sh
@@ -4,129 +4,41 @@ set -euo pipefail
 
 RESET="\033[0m"
 BOLD="\033[1m"
+UNDERLINE="\033[4m"
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
 BLUE="\033[34m"
 MAGENTA="\033[35m"
-CYAN="\033[36m"
 WHITE="\033[37m"
 
-# Download and install repository
-DOTFILES_DIR="$HOME/.dotfiles"
-if [[ -d "$DOTFILES_DIR" ]]; then
-  echo -e \
-    "${BOLD}${WHITE}Dotfiles directory already present. ${YELLOW}Override? ${BLUE}[y/u/q]\n" \
-    "   ${BOLD}${BLUE}y: ${RESET}yes\n" \
-    "   ${BOLD}${BLUE}u: ${RESET}update\n" \
-    "   ${BOLD}${BLUE}q: ${RESET}quit"
-  while true; do
-    read -r answer </dev/tty
-    case "$answer" in
-    y | Y)
-      rm -rf "$DOTFILES_DIR"
-      break
-      ;;
-    u | U)
-      cd "$DOTFILES_DIR"
-      git pull
-      exit 0
-      ;;
-    q | Q)
-      echo -e "${BOLD}${YELLOW}Abort...${RESET}"
-      exit 0
-      ;;
-    esac
-  done
-fi
-git clone https://github.com/0011101100101001/dotfiles.git "$DOTFILES_DIR"
-echo
+manage_config_install() {
+  CONFIG_PATH_SRC="$1"
+  CONFIG_PATH_DST="$2"
+  CONFIG_NAME="Config"
 
-# Check programs installation, setup config symlinks
-cd "$DOTFILES_DIR"
-CONFIG_DIR="$HOME/.config"
-mkdir -p "$CONFIG_DIR"
-for config_category in editor shell terminal; do
-  for config in $config_category/*; do
-    config=$(basename "$config")
-    if [[ "$config" = "alias.conf" ]] || command -v "$config" >/dev/null; then
-      CONFIG_PATH_DST="$CONFIG_DIR/$config"
-      CONFIG_PATH_SRC="$DOTFILES_DIR/$config_category/$config"
-      if [[ -d "$CONFIG_PATH_DST" || -f "$CONFIG_PATH_DST" ]]; then
-        echo -e \
-          "${BOLD}${MAGENTA}${config^} ${WHITE}already present, which action to perform? ${BLUE}[o/b/p]\n" \
-          "   ${BOLD}${BLUE}o: ${RESET}override\n" \
-          "   ${BOLD}${BLUE}b: ${RESET}backup\n" \
-          "   ${BOLD}${BLUE}p: ${RESET}pass"
-
-        while true; do
-          read -r answer </dev/tty
-          case "$answer" in
-          o | O)
-            rm -rf "$CONFIG_PATH_DST"
-            ln -sv "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
-            break
-            ;;
-          b | B)
-            mv "$CONFIG_PATH_DST" "$CONFIG_PATH_DST.bk"
-            ln -sv "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
-            break
-            ;;
-          p | P)
-            break
-            ;;
-          esac
-        done
-      else
-        ln -sv "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
-      fi
-    else
-      echo -e "${BOLD}${MAGENTA}${config^} ${WHITE}not installed.${RESET}"
-    fi
-    echo
-  done
-done
-
-# Setup aliases
-case "$(basename "$SHELL")" in
-"bash" | "zsh")
-  SHELLRC="$HOME/.$(basename "$SHELL")rc"
-  if ! grep "alias.conf" "$SHELLRC"; then
-    echo "source $DOTFILES_DIR/shell/alias.conf" >>"$SHELLRC"
-    source "$SHELLRC"
-  else
-    echo -e "${BOLD}${MAGENTA}Aliases ${WHITE}already setup.${RESET}"
+  if [[ $# -eq 3 ]]; then
+    CONFIG_NAME="$3"
   fi
-  ;;
 
-"fish")
-  SHELLRC="$HOME/.$(basename "$SHELL").fish"
-  if [[ -f "$HOME/.config/fish/alias.fish" ]]; then
-    rm -rf "$HOME/.config/fish/alias.fish"
-  fi
-  ;;
-esac
-echo
-
-# Setup fonts
-FONT_DIR_DST="$HOME/.local/share/fonts"
-mkdir -p "$FONT_DIR_DST"
-for font in font/*; do
-  FONT_PATH_SRC="$DOTFILES_DIR/$font"
-  font=$(basename $font)
-  FONT_PATH_DST="$FONT_DIR_DST/$font"
-  if [[ -d "$FONT_PATH_DST" ]]; then
+  if [[ -d "$CONFIG_PATH_DST" || -f "$CONFIG_PATH_DST" ]]; then
     echo -e \
-      "${BOLD}${MAGENTA}$(basename $font) ${WHITE}already present, which action to perform? ${BLUE}[o/p]\n" \
+      "${MAGENTA}${CONFIG_NAME} ${BOLD}${WHITE}already present. ${BLUE}[o/b/p]\n" \
       "   ${BOLD}${BLUE}o: ${RESET}override\n" \
+      "   ${BOLD}${BLUE}b: ${RESET}backup\n" \
       "   ${BOLD}${BLUE}p: ${RESET}pass"
 
     while true; do
       read -r answer </dev/tty
       case "$answer" in
       o | O)
-        rm -rf "$FONT_PATH_DST"
-        ln -sv "$FONT_PATH_SRC" "$FONT_PATH_DST"
+        rm -rf "$CONFIG_PATH_DST"
+        ln -sv "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
+        break
+        ;;
+      b | B)
+        mv "$CONFIG_PATH_DST" "$CONFIG_PATH_DST.bk"
+        ln -sv "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
         break
         ;;
       p | P)
@@ -135,28 +47,214 @@ for font in font/*; do
       esac
     done
   else
-    ln -sv "$FONT_PATH_SRC" "$FONT_PATH_DST"
+    ln -sv "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
   fi
+}
+
+echo -e "\n${BOLD}${BLUE}" \
+  "   ██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗\n" \
+  "   ██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝██╔════╝\n" \
+  "   ██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗  ███████╗\n" \
+  "   ██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║\n" \
+  "██╗██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║\n" \
+  "╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝\n"
+
+# Download and install repository
+DOTFILES_DIR="$HOME/.dotfiles"
+echo -e "${BOLD}${MAGENTA}${UNDERLINE}Dotfiles${RESET}"
+if [[ -d "$DOTFILES_DIR" ]]; then
+  echo -e \
+    "${MAGENTA}Directory ${BOLD}${WHITE}already present. ${BLUE}[o/u/q]\n" \
+    "   ${BOLD}${BLUE}o: ${WHITE}override\n" \
+    "   ${BOLD}${BLUE}u: ${WHITE}update\n" \
+    "   ${BOLD}${BLUE}q: ${WHITE}quit"
+  while true; do
+    read -r answer </dev/tty
+    case "$answer" in
+    o | O)
+      rm -rf "$DOTFILES_DIR"
+      git clone https://github.com/0011101100101001/dotfiles.git "$DOTFILES_DIR"
+      break
+      ;;
+    u | U)
+      echo -en "${BOLD}${GREEN}"
+      git -C "$DOTFILES_DIR" pull --ff-only
+      echo -en "${RESET}"
+      break
+      ;;
+    q | Q)
+      echo -e "${BOLD}${YELLOW}Abort...${RESET}"
+      exit 0
+      ;;
+    esac
+  done
+fi
+echo
+
+# Check programs installation, setup config symlinks
+cd "$DOTFILES_DIR"
+CONFIG_DIR="$HOME/.config"
+mkdir -p "$CONFIG_DIR"
+
+# Font
+echo -e "----------------------------------------"
+echo -e "${BOLD}${BLUE}" \
+  "╔═╗╔═╗╔╗╔╔╦╗\n" \
+  "╠╣ ║ ║║║║ ║ \n" \
+  "╚  ╚═╝╝╚╝ ╩\n${RESET}"
+
+readonly FONT_DIR_DST="$HOME/.local/share/fonts"
+mkdir -p "$FONT_DIR_DST"
+for font in font/*; do
+  FONT_PATH_SRC="$DOTFILES_DIR/$font"
+  font=${font##*/}
+  FONT_PATH_DST="$FONT_DIR_DST/$font"
+  echo -e "${BOLD}${MAGENTA}${UNDERLINE}$font${RESET}"
+  manage_config_install "$FONT_PATH_SRC" "$FONT_PATH_DST"
   echo
 done
 fc-cache -v "$FONT_DIR_DST"/* &>/dev/null
 
+# TODO: implement shell installation logic
+# echo -e "----------------------------------------"
+# echo -e "${BOLD}${BLUE}" \
+#   "╔═╗╦ ╦╔═╗╦  ╦  \n" \
+#   "╚═╗╠═╣║╣ ║  ║  \n" \
+#   "╚═╝╩ ╩╚═╝╩═╝╩═╝\n${RESET}"
+
+# Alias
+echo -e "----------------------------------------"
+echo -e "${BOLD}${BLUE}" \
+  "╔═╗╦  ╦╔═╗╔═╗\n" \
+  "╠═╣║  ║╠═╣╚═╗\n" \
+  "╩ ╩╩═╝╩╩ ╩╚═╝\n${RESET}"
+
+for config in alias/*; do
+  config=${config##*/}
+  echo -e "${BOLD}${MAGENTA}${UNDERLINE}Alias${RESET}"
+  ALIAS_PATH_DST="$CONFIG_DIR/$config"
+  ALIAS_PATH_SRC="$DOTFILES_DIR/alias/$config"
+  manage_config_install "$ALIAS_PATH_SRC" "$ALIAS_PATH_DST"
+done
+
+case ${SHELL##*/} in
+"bash" | "zsh")
+  SHELLRC="$HOME/.${SHELL##*/}rc"
+  if ! grep "alias.conf" "$SHELLRC"; then
+    echo "source $DOTFILES_DIR/alias/alias.conf" >>"$SHELLRC"
+  else
+    echo -e "${BOLD}${MAGENTA}Aliases ${WHITE}already setup.${RESET}"
+  fi
+  ;;
+
+"fish")
+  SHELLRC="$HOME/.${SHELL##*/}.fish"
+  if [[ -f "$HOME/.config/fish/alias.fish" ]]; then
+    rm -rf "$HOME/.config/fish/alias.fish"
+  fi
+  ;;
+esac
+echo
+
+# Terminal
+echo -e "----------------------------------------"
+echo -e "${BOLD}${BLUE}" \
+  "╔╦╗╔═╗╦═╗╔╦╗╦╔╗╔╔═╗╦  \n" \
+  " ║ ║╣ ╠╦╝║║║║║║║╠═╣║  \n" \
+  " ╩ ╚═╝╩╚═╩ ╩╩╝╚╝╩ ╩╩═╝\n${RESET}"
+
+for config in terminal/*; do
+  config=${config##*/}
+  echo -e "${BOLD}${MAGENTA}${UNDERLINE}${config^}${RESET}"
+  if command -v "$config" >/dev/null; then
+    CONFIG_PATH_DST="$CONFIG_DIR/$config"
+    CONFIG_PATH_SRC="$DOTFILES_DIR/terminal/$config"
+    manage_config_install "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
+  else
+    echo -e "${BOLD}${WHITE}Not installed.${RESET}"
+  fi
+  echo
+done
+
+# Editor
+echo -e "----------------------------------------"
+echo -e "${BOLD}${BLUE}" \
+  "╔═╗╔╦╗╦╔╦╗╔═╗╦═╗\n" \
+  "║╣  ║║║ ║ ║ ║╠╦╝\n" \
+  "╚═╝═╩╝╩ ╩ ╚═╝╩╚═\n${RESET}"
+
+for config in editor/*; do
+  config=${config##*/}
+  echo -e "${BOLD}${MAGENTA}${UNDERLINE}${config^}${RESET}"
+  if command -v "$config" >/dev/null; then
+    CONFIG_PATH_DST="$CONFIG_DIR/$config"
+    CONFIG_PATH_SRC="$DOTFILES_DIR/editor/$config"
+    manage_config_install "$CONFIG_PATH_SRC" "$CONFIG_PATH_DST"
+  else
+    echo -e "${BOLD}${WHITE}Not installed.${RESET}"
+  fi
+  echo
+done
+
+# AI
+echo -e "----------------------------------------"
+echo -e "${BOLD}${BLUE}" \
+  "╔═╗╦\n" \
+  "╠═╣║\n" \
+  "╩ ╩╩\n${RESET}"
+
+AI_CONFIGS_DIR_DST=(
+  "$HOME/.claude"
+  "$HOME/.codex"
+  "$HOME/.config/opencode"
+)
+
+i=0
+for config_dir in ai/*; do
+  program=${config_dir##*/}
+  echo -e "${BOLD}${MAGENTA}${UNDERLINE}${program^}${RESET}"
+  if command -v $program &>/dev/null; then
+    for config_file in $config_dir/*; do
+      AI_CONFIGS_PATH_DST="${AI_CONFIGS_DIR_DST[$i]}/${config_file##*/}"
+      AI_CONFIGS_PATH_SRC="$DOTFILES_DIR/$config_file"
+      manage_config_install "$AI_CONFIGS_PATH_SRC" "$AI_CONFIGS_PATH_DST" "${config_file##*/}"
+      echo
+    done
+  else
+    echo -e "${BOLD}${WHITE}Not installed.${RESET}"
+  fi
+  ((++i))
+done
+
+# TODO: implement boot installation logic
+# echo -e "----------------------------------------"
+# echo -e "${BOLD}${BLUE}" \
+#   "╔╗ ╔═╗╔═╗╔╦╗\n" \
+#   "╠╩╗║ ║║ ║ ║ \n" \
+#   "╚═╝╚═╝╚═╝ ╩${RESET}"
+
 # Setup dotfiles binary
+echo -e "----------------------------------------"
+echo -e "${BOLD}${BLUE}" \
+  "╔╗ ╦╔╗╔╔═╗╦═╗╦ ╦\n" \
+  "╠╩╗║║║║╠═╣╠╦╝╚╦╝\n" \
+  "╚═╝╩╝╚╝╩ ╩╩╚═ ╩\n${RESET}"
+
 DOTFILES_BIN_DIR="$HOME/.local/bin"
 DOTFILES_BIN="$DOTFILES_BIN_DIR/dotfiles"
 if command -v dotfiles >/dev/null; then
   echo -e \
-    "${BOLD}${WHITE}Dotfiles binary already installed. ${YELLOW}Override? ${BLUE}[y/n]\n" \
-    "   ${BOLD}${BLUE}y: ${RESET}yes\n" \
-    "   ${BOLD}${BLUE}n: ${RESET}no"
+    "${BOLD}${WHITE}Dotfiles binary already installed. ${BLUE}[o/p]\n" \
+    "   ${BOLD}${BLUE}o: ${RESET}override\n" \
+    "   ${BOLD}${BLUE}p: ${RESET}pass"
   while true; do
     read -r answer </dev/tty
     case "$answer" in
-    y | Y)
+    o | O)
       rm -f "$DOTFILES_BIN"
       break
       ;;
-    n | N)
+    p | P)
       echo -e "${BOLD}${GREEN}Done!${RESET}"
       exit 0
       ;;
@@ -191,13 +289,12 @@ case "$1" in
     git pull
     ;;
 
-  default)
+  *)
     echo "Invalid option: $1"
     echo "$USAGE"
     exit 1
     ;;
 esac' >"$DOTFILES_BIN"
-
 chmod u+x "$DOTFILES_BIN"
 
-echo -e "${BOLD}${GREEN}Done!${RESET}"
+echo -e "\n${BOLD}${GREEN}Done!${RESET}"
